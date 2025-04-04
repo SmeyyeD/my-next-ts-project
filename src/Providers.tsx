ab5488736303629b9a07661a1e315c0c2b05046c
Sümeyye Danışmaz <smeyyedansmzz@gmail.com>
2025-04-04 16:33:52 +0300

    store-done

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,14 +1,14 @@
 "use client";
 
 import React from "react";
-import { SWRConfig } from "swr";
 import { fetcher } from "./app/services/api";
 import { Provider } from "react-redux";
 import AuthWrapper from "./app/(auth)/components/AuthWrapper"; 
+import { SWRConfig } from "swr";
 
 function Providers({ children }: { children: React.ReactNode }) {
     return (
-        <Provider store={store}>
+        <Provider store ={store}>
             <AuthWrapper>
                 <SWRConfig value={{ fetcher }}>{children}</SWRConfig>
             </AuthWrapper>
